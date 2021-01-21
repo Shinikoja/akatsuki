d41f7cc5710367762d9ea02e12c455aa87bd1be6
Shini <48142979+Shinikoja@users.noreply.github.com>
2021-01-21 01:41:16 +0000

    Add files via upload

---
+++ commands/moderation/purge.js
@@ -0,0 +1,38 @@
+const { Command } = require('discord.js-commando');
+const { RichEmbed } = require('discord.js');
+
+module.exports = class extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'purge',
+            group: 'moderation',
+            memberName: 'purge',
+            description: 'Purges a specified amount of messages',
+            aliases: ['p'],
+            userPermissions: ['MANAGE_MESSAGES'],
+            clientPermissions: ['MANAGE_MESSAGES'],
+            guildOnly: true
+        });
+    }
+
+   async run(message) {
+       const args = message.content.split(' ');
+       if (args.length == 1) {
+           message.channel.fetchMessages({ limit: 2 }).then(messages => {
+               message.channel.bulkDelete(messages);
+           });
+       } else {
+           const amount = parseInt(args[1]);
+           if (amount > 100) {
+               return message.embed(new RichEmbed()
+                    .setColor('RED')
+                    .setTitle('Please specify a smaller value')
+                );
+           } else {
+               return message.channel.fetchMessages({ limit: amount + 1 }).then(messages => {
+                    message.channel.bulkDelete(messages);
+                });
+            }
+        }
+    }
+};
