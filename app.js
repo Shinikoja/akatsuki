66ab4438db8b231ee6860c752b6ac56eed650d7c
Shini <48142979+Shinikoja@users.noreply.github.com>
2021-03-13 03:55:30 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const Discord = require('discord.js');
+const .js');
 const Commando = require('discord.js-commando');
 const path = require('path');
 const sqlite = require('sqlite');
